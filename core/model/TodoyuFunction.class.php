1397fa02e6e16c83adcb375f75fb26ea688596e2
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-05-19 17:22:21 +0800

    整理和修正导致服务器500错误BUG

--- core/model/TodoyuFunction.class.php
+++ core/model/TodoyuFunction.class.php
@@ -1,28 +1,28 @@
 <?php
 /****************************************************************************
-* todoyu is published under the BSD License:
-* http://www.opensource.org/licenses/bsd-license.php
-*
-* Copyright (c) 2012, snowflake productions GmbH, Switzerland
-* All rights reserved.
-*
-* This script is part of the todoyu project.
-* The todoyu project is free software; you can redistribute it and/or modify
-* it under the terms of the BSD License.
-*
-* This script is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
-* for more details.
-*
-* This copyright notice MUST APPEAR in all copies of the script.
-*****************************************************************************/
+ * todoyu is published under the BSD License:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Copyright (c) 2012, snowflake productions GmbH, Switzerland
+ * All rights reserved.
+ *
+ * This script is part of the todoyu project.
+ * The todoyu project is free software; you can redistribute it and/or modify
+ * it under the terms of the BSD License.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+ * for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script.
+ *****************************************************************************/
 
 /**
  * Function handling
  *
- * @package		Todoyu
- * @subpackage	Core
+ * @package        Todoyu
+ * @subpackage     Core
  */
 class TodoyuFunction {
 
@@ -31,66 +31,66 @@ class TodoyuFunction {
 	 * string function reference like 'MyClass::myMethod'
 	 * First argument is the function reference, all others are normal parameters passed to the function
 	 *
-	 * @param	String		$funcRefString		Function reference
-	 * @return	Mixed
+	 * @param    String $funcRefString        Function reference
+	 *
+	 * @return    Mixed
 	 */
 	public static function callUserFunction($funcRefString) {
-		$funcArgs	= func_get_args();
+		$funcArgs = func_get_args();
 		array_shift($funcArgs); // Remove function name
 
-		if( self::isFunctionReference($funcRefString) ) {
-			$funcRefParts = explode('::', $funcRefString);
-
-			$result	= call_user_func_array($funcRefParts, $funcArgs);
+		//if( self::isFunctionReference($funcRefString) ) {
+		$funcRefParts = explode('::', $funcRefString);
+		if(isset($funcRefParts[1]) && method_exists($funcRefParts[0], $funcRefParts[1]) || !isset($funcRefParts[1]) && function_exists($funcRefParts[0])) {
+			$result = call_user_func_array($funcRefParts, $funcArgs);
 		} else {
-			TodoyuLogger::logError('Call to undefined function: <' . $funcRefString . '>');
+			TodoyuLogger::logError('Call to undefined function: <'.$funcRefString.'>');
 
-			$result = false;
+			$result = FALSE;
 		}
 
 		return $result;
 	}
-
-
 
 	/**
 	 * Call user function where parameters are stored in an array
-	 * @see		callUserFunction()
-	 * @see		call_user_func_array()
 	 *
-	 * @param	String		$funcRefString
-	 * @param	Array		$funcArgs
-	 * @return	Mixed
+	 * @see        callUserFunction()
+	 * @see        call_user_func_array()
+	 *
+	 * @param    String $funcRefString
+	 * @param    Array  $funcArgs
+	 *
+	 * @return    Mixed
 	 */
 	public static function callUserFunctionArray($funcRefString, array $funcArgs) {
-		if( self::isFunctionReference($funcRefString) ) {
+		if(self::isFunctionReference($funcRefString)) {
 			$funcRefParts = explode('::', $funcRefString);
 
-			$result	= call_user_func_array($funcRefParts, $funcArgs);
+			$result = call_user_func_array($funcRefParts, $funcArgs);
 		} else {
-			TodoyuLogger::logFatal('Function not found: ' . $funcRefString);
-			$result = false;
+			TodoyuLogger::logFatal('Function not found: '.$funcRefString);
+			$result = FALSE;
 		}
 
 		return $result;
 	}
-
-
 
 	/**
 	 * Check if a function/method reference is valid
 	 *
-	 * @param	String		$funcRefString		Format: function or class::method
-	 * @return	Boolean
+	 * @param    String $funcRefString        Format: function or class::method
+	 *
+	 * @return    Boolean
 	 */
 	public static function isFunctionReference($funcRefString) {
-		if( !is_string($funcRefString) ) {
-			return false;
+		if(!is_string($funcRefString)) {
+			return FALSE;
 		}
-		if( strpos($funcRefString, '::') === false ) {
+		if(strpos($funcRefString, '::') === FALSE) {
 			return function_exists($funcRefString);
 		} else {
-			$parts	= explode('::', $funcRefString);
+			$parts = explode('::', $funcRefString);
 
 			return method_exists($parts[0], $parts[1]);
 		}
