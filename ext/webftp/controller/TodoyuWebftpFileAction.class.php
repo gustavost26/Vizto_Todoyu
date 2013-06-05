da80c7cb02c100337e8d6222a6ccfb6ae43c931c
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-06-05 19:09:55 +0800

    页面处理类

---
+++ ext/webftp/controller/TodoyuWebftpFileAction.class.php
@@ -0,0 +1,61 @@
+<?php
+/****************************************************************************
+ * todoyu is published under the BSD License:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Copyright (c) 2011, snowflake productions GmbH, Switzerland
+ * All rights reserved.
+ *
+ * This script is part of the todoyu project.
+ * The todoyu project is free software; you can redistribute it and/or modify
+ * it under the terms of the BSD License.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+ * for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script.
+ *****************************************************************************/
+
+/**
+ * Ext action controller
+ *
+ * @package		Todoyu
+ * @subpackage	Webftp
+ */
+class TodoyuWebftpFileActionController extends TodoyuActionController {
+	public function listAction(array $params){
+		//l
+	}
+	public function propertyAction(array $params){
+		//nh
+	}
+	public function renameAction(array $params){
+		//
+	}
+	public function downloadfileAction(array $params){
+		//
+	}
+	public function unzipAction(array $params){
+
+	}
+	public function zipAction(array $params){
+
+	}
+	public function deleteAction(array $params){
+
+	}
+	public function pasteAction(array $params){
+		//
+	}
+	public function chmodAction(array $params){
+		//
+	}
+	public function newbulidAction(array $params){
+		//
+	}
+	public function imageviewAction(array $params){
+		//
+	}
+}
