ed85e72a2c0c47f1feac68275558afe3c02fbee0
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-06-05 19:08:28 +0800

    添加SCSS资源

--- ext/webftp/config/assets.php
+++ ext/webftp/config/assets.php
@@ -18,6 +18,11 @@ Todoyu::$CONFIG['EXT']['webftp']['assets'] = array(
 		array(
 			'file'		=> 'ext/webftp/asset/css/ext.css',
 			'media'		=> 'all'
+		),
+		array(
+			'file'		=> 'ext/webftp/asset/css/panelwidget-fileoperation.scss',
+			'media'		=> 'all',
+			'opsition'  => 110
 		)
 	)
 );
