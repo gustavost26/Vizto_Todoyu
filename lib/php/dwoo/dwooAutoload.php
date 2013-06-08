d223b53bb05a62a279bc01d23075daeef6743e55
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-06-08 13:36:59 +0800

    修改重复导入

--- lib/php/dwoo/dwooAutoload.php
+++ lib/php/dwoo/dwooAutoload.php
@@ -1,14 +1,12 @@
 <?php
 function dwooAutoload($class) {
-	if($class==='Dwoo') {
+	if($class === 'Dwoo') {
 		include dirname(__FILE__).DIRECTORY_SEPARATOR.'Dwoo.compiled.php';
 		include(PATH_CORE.'/model/dwoo/plugins.php');
-	}
-	if(substr($class, 0, 20) === 'Dwoo_Plugin_restrict') {
+	} elseif(substr($class, 0, 20) === 'Dwoo_Plugin_restrict') {
 		$file = PATH_CORE.'/model/dwoo/'.$class.'.php';
 		is_file($file) && include($file);
-	}
-	if(substr($class, 0, 5) === 'Dwoo_') {
+	} elseif(substr($class, 0, 5) === 'Dwoo_') {
 		include DWOO_DIRECTORY.strtr($class, '_', DIRECTORY_SEPARATOR).'.php';
 	}
 }
